6f9c2cf37400881f042b14c3a97fc32d563c61eb
bve81 <eschenko@live.ru>
2022-04-11 20:35:26 +0300

    function GistInfo allmost done

--- client/pages/gist.js
+++ client/pages/gist.js
@@ -1,9 +1,27 @@
 import Layout from "../components/Layout";
+import React, { useEffect, useState } from "react";
+import getGistInfoById from '../utils/gitApi/getGistInfoById.js';
 
 //TODO: Здесь должна быть форма которая принимает id гиста, вызывает функцию getGistInfoById из utils/gitAPI/getGistInfoById и результат выполнения отображает под формой
 
 const Gist = (props) => {
+    const [gistid, setGistid] = useState('');
+    function handleOnSubmintForm(e) {
+        alert(gistid);
+        getGistInfoById(gistid);
+        e.preventDefault();
+      }
+    
+
     return ( <Layout>
+<form onSubmit={handleOnSubmintForm}>
+  <label>
+    GistID:
+    <input type="text" name="name" value={gistid} onChange={(e) => setGistid(e.target.value)}/>
+  </label>
+  <input type="submit" value="Отправить" />
+</form>
+
         {/* Код писать здесь */}
     </Layout> );
 }
--- client/utils/gitApi/getGistInfoById.js
+++ client/utils/gitApi/getGistInfoById.js
@@ -1,5 +1,16 @@
 //TODO: Функция должны подключаться к api github.com и возвращать объект вида {creator: ник создателя гиста, text: текст гиста, lastUpdate: дата последней модификации, }
+import axios from "axios";
 
 const getGistInfoById = async (gistId) => {
+let id = gistId;
+let info = axios.get(`https://api.github.com/gists/${id}`)
+.then(res => {
+    let info = res.data;
+      return (info);}); 
+const owner = info.then(result => result)
+.then((result) =>{let ownerInfo = {creator: result.owner.login, text: result.files, lastUpdate: result.updated_at }; return ownerInfo});
+
+return (owner);
 
 }
+export default getGistInfoById;
