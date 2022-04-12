beca56f724a061f9508f9408ed0e589eca256843
bve81 <eschenko@live.ru>
2022-04-12 22:36:26 +0300

    master after refactoring

--- client/pages/gist.js
+++ client/pages/gist.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import React, { useEffect, useState } from "react";
 import getGistInfoById from '../utils/gitApi/getGistInfoById.js';
+import { Button, Message, List, Form } from 'semantic-ui-react';
 
 //TODO: Здесь должна быть форма которая принимает id гиста, вызывает функцию getGistInfoById из utils/gitAPI/getGistInfoById и результат выполнения отображает под формой
 
@@ -9,28 +10,34 @@ const Gist = (props) => {
     const [creator, setCreator] = useState([]);
     const [text, setText] = useState([]);
     const [lastupdate, setLastupdate] = useState([]);
+    const [visible, setVisible] = useState(false);
   
     function handleOnSubmintForm(e) {
         let userInfo = getGistInfoById(gistid);
-        userInfo.then(result=>result).then ((result) =>{ setCreator(result.creator); setText(result.text); setLastupdate(result.lastUpdate); });
+        userInfo.then ((result) =>{ setCreator(result.creator); setText(result.text); setLastupdate(result.lastUpdate); });
+        setVisible(true);
         e.preventDefault();
       }
+
+    const ShowInfo = () => (
+
+  <List>
+    <List.Item><List.Content><List.Header>Login</List.Header><List.Description>{creator}</List.Description></List.Content></List.Item>
+    <List.Item><List.Content><List.Header>Text</List.Header><List.Description>{text}</List.Description></List.Content></List.Item>
+    <List.Item><List.Content><List.Header>Last Update</List.Header><List.Description>{lastupdate}</List.Description></List.Content></List.Item>
+  </List>
+    );
     
     return ( <Layout>
-<div>
- <ul>
-     <li>{creator}</li>
-     <li>{text}</li>
-     <li>{lastupdate}</li>
- </ul>
-<form onSubmit={handleOnSubmintForm}>
-  <label>
-    GistID:
-    <input type="text" name="name" value={gistid} onChange={(e) => setGistid(e.target.value)}/>
-  </label>
-  <input type="submit" value="Отправить" />
-</form>
-</div>
+    <Form onSubmit={handleOnSubmintForm}>
+    <Form.Field>
+      <label>GistID</label>
+      <input placeholder='Your GistID' value={gistid} onChange={(e) => setGistid(e.target.value)}/>
+    </Form.Field>
+   
+    <Button type='submit'>Submit</Button>
+  </Form>
+  {visible && <ShowInfo />} 
         {/* Код писать здесь */}
     </Layout> );
 }
--- client/utils/gitApi/getGistInfoById.js
+++ client/utils/gitApi/getGistInfoById.js
@@ -1,16 +1,11 @@
 //TODO: Функция должны подключаться к api github.com и возвращать объект вида {creator: ник создателя гиста, text: текст гиста, lastUpdate: дата последней модификации, }
-import axios from "axios";
-
 const getGistInfoById = async (gistId) => {
-let id = gistId;
-let info = axios.get(`https://api.github.com/gists/${id}`)
-.then(res => {
-    let info = res.data;
-      return (info);}); 
-const owner = info.then(result => result)
-.then((result) =>{  
-    let fileName = Object.values(result.files);
-    let ownerInfo = {creator: result.owner.login, text: fileName[0].content, lastUpdate: result.updated_at }; return ownerInfo});
+    const response = await fetch(`https://api.github.com/gists/${gistId}`);
+    const data = await response.json();
+    let fileName = Object.values(data.files);
+    let owner = {creator: data.owner.login, text: fileName[0].content, lastUpdate: data.updated_at };
+    console.log(fileName[0].content);
+
 return (owner);
 
 }
