afce369d5aa96191be939b62fac5547ab4bf4cf7
bve81 <eschenko@live.ru>
2022-04-13 21:06:06 +0300

    catch

--- client/utils/gitApi/getGistInfoById.js
+++ client/utils/gitApi/getGistInfoById.js
@@ -1,6 +1,7 @@
 //TODO: Функция должны подключаться к api github.com и возвращать объект вида {creator: ник создателя гиста, text: текст гиста, lastUpdate: дата последней модификации, }
 const getGistInfoById = async (gistId) => {
-    const response = await fetch(`https://api.github.com/gists/${gistId}`);
+    const response = await fetch(`https://api.github.com/gists/${gistId}`)
+    .catch((error) => alert(error));
     const data = await response.json();
     let fileName = Object.values(data.files);
     let owner = {creator: data.owner.login, text: fileName[0].content, lastUpdate: data.updated_at };
